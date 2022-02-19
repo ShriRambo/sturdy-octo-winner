1933f96f68d763203db352ff19f2d1658b0088f8
shriram <sashirgade@gmail.com>
2022-02-19 11:30:10 +0100

    Build.sh folder copy bug. Making dir before copy.

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo Copying all the resources
-cp -r ./resources/css/. ./public/css/
-cp -r ./resources/js/.  ./public/js/
+mkdir -p ./public/css ./public/js
+cp -r ./resources/css/. ./public/css
+cp -r ./resources/js/.  ./public/js
 
 echo Building web pages
 emacs -Q --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "build.org")'
