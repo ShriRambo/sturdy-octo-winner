2ce02a7012ae1df9fb9d600af4875cdcec635394
shriram <sashirgade@gmail.com>
2022-02-19 11:13:17 +0100

    Code highliting css and code block formatting.

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo Copying all the resources
+cp -r ./resources/css/. ./public/css/
+cp -r ./resources/js/.  ./public/js/
+
+echo Building web pages
 emacs -Q --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "build.org")'
 emacs -Q --script build.el
 rm build.el
