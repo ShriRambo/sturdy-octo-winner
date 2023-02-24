7595d1e8a724595d62d9b5955e9f501d696dad0f
shriram <sashirgade@gmail.com>
2023-02-24 17:52:54 +0100

    Preamble format page header

--- clean-external.sh
+++ clean-external.sh
@@ -1,2 +1,2 @@
-rm "./content/publish-settings.org" "./content/paperless-ngx-config.org" "./content/emacs-config.org" "./content/adguardhome-config.org"
+rm "./content/publish-settings.org" "./content/paperless-ngx-config.org" "./content/emacs-config.org" "./content/adguardhome-config.org" "./content/index.org"
 
